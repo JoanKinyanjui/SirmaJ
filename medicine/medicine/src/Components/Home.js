aa95fb674c122f766443a73064a0fe769764b97d
Joan Knyanjui <kinyanjuijoan52@gmail.com>
2021-10-12 21:47:48 +0300

    final

--- medicine/medicine/src/Components/Home.js
+++ medicine/medicine/src/Components/Home.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import Nav from './Navbar/nav';
 import Works from './About/Works';
 import About from './About/About';
 import Doctors from './Our Doctors/Doctors';
@@ -7,9 +6,8 @@ import Service from './Our Services/Service';
 import Purchase from './Purchase/purchase';
 import Reviews from './carousel/carousel';
 import Footer from './Footer/Footer';
-import Cart from './Cart/Cart';
-import {CartProvider }from 'react-use-cart';
 import Popup from './Popup/Popup';
+import Nav from './Navbar/nav';
 
 
 function Home(){
@@ -37,10 +35,7 @@ useEffect( ()=>{
        <Works />
        <Doctors />
        <Service/>
-       <CartProvider>
       <Purchase />
-      <Cart />
-      </CartProvider>
       <Reviews />
        <Footer />
 
--- medicine/medicine/src/Components/Navbar/nav.js
+++ medicine/medicine/src/Components/Navbar/nav.js
@@ -1,16 +1,17 @@
-import React from 'react';
+import React ,{useState}from 'react';
 import { NavItems } from './Navitems';
 import '../Navbar/Nav.css';
 import {Link} from 'react-router-dom';
-import { Badge, IconButton } from '@material-ui/core';
-import { ShoppingCart } from '@material-ui/icons';
+
+ 
 
 
 function Nav(){
     return(
         <div className='Nav'>
+             <div><h2 className='glow'>Gloww</h2></div>
             <nav className='navbar navbar-light'>
-                <div><h2>DOCTOR HERB  <i class="fas fa-leaf"></i></h2></div>
+ 
                <div className='nav-items'>
                  <ul className='nav'>
                      {NavItems.map((item,id)=>{
@@ -23,6 +24,7 @@ function Nav(){
                          )}
                  </ul>
                </div>
+              
             </nav>
 
         </div>
